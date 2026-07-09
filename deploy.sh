a1bc084a59212623ec29c51b64eee952e1abbeb1
Mukhammed <muhammatqudratov@gmail.com>
2026-07-09 21:17:15 +0900

    fix: deploy folder

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,14 @@ npm i yarn -g
 yarn global add serve
 yarn  
 yarn build
-pm2 start "yarn run start:prod" --name=AlphaZone-React 
 
+pm2 restart AlphaZone-React || pm2 start "yarn run start:prod" --name=AlphaZone-React
+pm2 save
 
 #DEVELOPMENT
 # git reset --hard
 # git checkout develop
 # git pull origin develop
 # yarn install
-# pm2 start "yarn run start:dev" --name=AlphaZone 
-
+# pm2 restart AlphaZone || pm2 start "yarn run start:dev" --name=AlphaZone
+# pm2 save
